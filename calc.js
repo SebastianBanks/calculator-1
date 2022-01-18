9db42f6c7bf6235a94d974fe08222f962fcd15a4
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-01-18 14:35:58 -0700

    added square root operator

--- calc.js
+++ calc.js
@@ -14,6 +14,7 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	let isSquareRoot = false
 	let answer
 
 	if (mathSymbol === "+") {
@@ -28,12 +29,19 @@ reader.question("What would you like to calculate?", function(input){
 		answer = num1 % num2
 	} else if(mathSymbol === "^") {
 		answer = Math.pow(num1, num2)
-	} else {
+	} else if(mathSymbol === "sqrt") {
+		isSquareRoot = true
+		answer = Math.sqrt(num1)
+	}else {
 		console.log(`Unexpected mathSymbol: ${mathSymbol}`)
 	}
 	
-
-	console.log(`${num1} ${mathSymbol} ${num2} = ${answer}`)
+	if (isSquareRoot === false) {
+		console.log(`${num1} ${mathSymbol} ${num2} = ${answer}`)
+	} else {
+		console.log(`The square root of ${num1} = ${answer}`)
+	}
+	
 
 	// This line closes the connection to the command line interface.
 	reader.close()
