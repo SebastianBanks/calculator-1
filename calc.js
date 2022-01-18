dba9190330070c21703da5eada4a5a49440c3ab9
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-01-18 14:26:56 -0700

    basic operators working

--- calc.js
+++ calc.js
@@ -14,6 +14,26 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	let answer
+
+	if (mathSymbol === "+") {
+		answer = num1 + num2
+	} else if (mathSymbol === "-") {
+		answer = num1 - num2
+	} else if (mathSymbol === "*") {
+		answer = num1 * num2
+	} else if (mathSymbol === "/") {
+		answer = num1 / num2
+	} else if (mathSymbol === "%") {
+		answer = num1 % num2
+	} else if(mathSymbol === "^") {
+		answer = Math.pow(num1, num2)
+	} else {
+		console.log(`Unexpected mathSymbol: ${mathSymbol}`)
+	}
+	
+
+	console.log(`${num1} ${mathSymbol} ${num2} = ${answer}`)
 
 	// This line closes the connection to the command line interface.
 	reader.close()
